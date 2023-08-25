e4287b4fd7e5616bdeb8e883109802f66991d077
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-08-25 10:31:51 +0530

    Add files via upload
    the program to check NO is Even or odd

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Enetr the value of a:";
+	cin >> a;
+	if(a%2==0)
+	{
+		cout<< a << " is a even number";
+		
+	}
+	else
+	{
+		cout<< a << " is not a even number";
+	}
+	return 0;
+}
