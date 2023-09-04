4f799909dd49b9ea2770708c4f2875d416eda04b
Shubham Palde <114591652+paldeshubham@users.noreply.github.com>
2023-09-04 13:09:45 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j;
+	int a[10] ={0,5,60,59,65,47,25,2,4,1};
+	
+	
+	
+	for(i=0;i<10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if (a[i]>a[j])
+			{
+				swap(a[i],a[j]);
+				
+			}
+		}
+	}
+	cout << "Sorted Array is:\n";
+	for(i=0;i<10;i++)
+	{
+		cout << a[i] << "\t";
+	}
+}
